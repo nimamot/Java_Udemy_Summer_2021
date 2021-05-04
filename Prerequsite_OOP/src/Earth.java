66cb275c6fa1c305311cd5bdafe90c7c67666074
nimamot <64922998+nimamot@users.noreply.github.com>
2021-05-04 13:27:11 -0700

    first commit

---
+++ Prerequsite_OOP/src/Earth.java
@@ -0,0 +1,17 @@
+
+public class Earth {
+	
+	public static void main(String args[]) {
+		Human human1 = new Human("Tom", 5, 72, "Brown");
+		Human human2 = new Human("Joe", 15, 92, "Red");
+
+		human1.speak();
+		human2.speak();
+
+		
+		
+		
+		
+	}
+
+}
---
+++ Prerequsite_OOP/src/Human.java
@@ -0,0 +1,46 @@
+
+public class Human {
+	
+	
+	
+	//attributes
+	String name;
+	int age;
+	int heightInInches;
+	String eyeColor;
+	
+	public Human(String name, int age, int heightInInches, String eyeColor) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.heightInInches = heightInInches;
+		this.eyeColor = eyeColor;
+	}
+
+
+	
+	// Methods
+	public void speak() {
+		System.out.println("Hello my name is " + name);
+		System.out.println("I am " + heightInInches + " inches");
+		System.out.println("I am " + age  + " years old");
+		System.out.println("My eye color is " + eyeColor);
+
+	}
+	
+	public void eat() {
+		System.out.println("eating...");
+		
+	}
+		
+	public void walk() {
+		System.out.println("Walking");
+	}
+	
+	public void work() {
+		System.out.println("Working");
+		
+	}
+}
+	
+	
